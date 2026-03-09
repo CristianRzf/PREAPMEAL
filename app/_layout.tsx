2358dec1bbcb5d3f986d162fd81d0430fbc43de6
Cristian Felipe Ruiz Arias <cristian.ruiz03@usc.edu.co>
2026-03-09 11:25:18 -0500

    conexion firebase login

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,38 @@
 import { Stack } from "expo-router";
+import { useEffect, useState } from "react";
+import auth, { FirebaseAuthTypes } from "@react-native-firebase/auth";
+import { ActivityIndicator, View } from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null);
+
+  function onAuthStateChanged(user: FirebaseAuthTypes.User | null) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber;
+  }, []);
+
+  if (initializing) {
+    return (
+      <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
+        <ActivityIndicator size="large"/>
+      </View>
+    );
+  }
+
+  return (
+    <Stack screenOptions={{ headerShown:false }}>
+      {user ? (
+        <Stack.Screen name="(app)" />
+      ) : (
+        <Stack.Screen name="(auth)" />
+      )}
+    </Stack>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,94 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+
+import {
+  ActivityIndicator,
+  Button,
+  KeyboardAvoidingView,
+  TextInput,
+  View
+} from "react-native";
+
+import { StyleSheet } from "react-native";
+//
+import auth from "@react-native-firebase/auth";
+import { FirebaseError } from "firebase/app";
 
 export default function Index() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const signUp = async () => {
+    setLoading(true);
+    try {
+      await auth().createUserWithEmailAndPassword(email, password);
+      alert("Check your emails");
+    } catch (e: any) {
+      const err = e as FirebaseError;
+      alert("El registro fallo: " + err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const signIn = async () => {
+    setLoading(true);
+    try {
+      await auth().signInWithEmailAndPassword(email, password);
+    } catch (e: any) {
+      const err = e as FirebaseError;
+      alert("El sign in fallo: " + err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <KeyboardAvoidingView behavior="padding">
+
+        <TextInput
+          value={email}
+          style={styles.input}
+          onChangeText={setEmail}
+          autoCapitalize="none"
+          keyboardType="email-address"
+          placeholder="Email"
+        />
+
+        <TextInput
+          value={password}
+          style={styles.input}
+          onChangeText={setPassword}
+          secureTextEntry
+          placeholder="Password"
+        />
+
+        {loading ? (
+          <ActivityIndicator size="small" style={{ margin: 28 }} />
+        ) : (
+          <>
+            <Button onPress={signUp} title="Sign Up" />
+            <Button onPress={signIn} title="Sign In" />
+          </>
+        )}
+
+      </KeyboardAvoidingView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 20
+  },
+  input: {
+    height: 40,
+    borderWidth: 1,
+    marginBottom: 10,
+    padding: 10
+  }
+});
